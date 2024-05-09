ed529d714354eb5a62e73983817ba0770722d3eb
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2024-05-09 15:59:34 +0100

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+	return (
+		<main className='font-ubuntu bg-purplish-blue'>
+			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+		</main>
+	);
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'ubuntu': ['Ubuntu', 'sans'],
+			},
+			colors: {
+				'marine-blue': 'hsl(var(--marine-blue)/<alpha-value>)',
+				'purplish-blue': 'hsl(var(--purplish-blue)/<alpha-value>)',
+				'pastel-blue': 'hsl(var(--pastel-blue)/<alpha-value>)',
+				'light-blue': 'hsl(var(--light-blue)/<alpha-value>)',
+				'strawberry-red': 'hsl(var(--strawberry-red)/<alpha-value>)',
+				'cool-gray': 'hsl(var(--cool-gray)/<alpha-value>)',
+				'light-gray': 'hsl(var(--light-gray)/<alpha-value>)',
+				'magnolia': 'hsl(var(--magnolia)/<alpha-value>)',
+				'alabaster': 'hsl(var(--alabaster)/<alpha-value>)',
+			},
+		},
+	},
+	plugins: [],
+};
